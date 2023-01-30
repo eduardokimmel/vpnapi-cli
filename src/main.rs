29bd076bb6526c14f1971758b55eefda0c80bd84
eduardo <eduardo@localhost>
2023-01-30 18:42:52 -0300

    Added pprint and configured vpnapiresult struct

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,83 @@
 #![feature(ip)]
 use std::{net::{IpAddr, Ipv4Addr, Ipv6Addr, AddrParseError}, str::FromStr};
 use clap::Parser;
-use serde::Deserialize;
-// use  serde::{Deserialize, "derive"};
+use serde::{Serialize, Deserialize};
 
 
 #[derive(Parser,Default,Debug)]
 struct Arguments {
     ip: String,
+
+    /// vpnapi.io key of your account
     #[clap(default_value = "",short, long)]
     key: String,
+
+    /// Reads a list of IPs 
     #[clap(short, long)]
     file: Option<String>,
+
+    /// Saves the API key so you don't need to enter every time
     #[clap(short, long)]
-    config: Option<String>
+    config: Option<String>,
+
+    /// Pretty prints the results
+    #[clap(short, long)]
+    pprint: bool,
 }
+
+#[derive(Deserialize,Serialize, Debug)]
+struct VpnApiResult {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    ip: Option<String>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    security: Option<Security>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    location: Option<Location>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    network: Option<Network>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message: Option<String>,
+    
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Deserialize,Serialize, Debug)]
+struct Security {
+    vpn: Option<bool>,
+    proxy: Option<bool>,
+    tor: Option<bool>,
+    relay: Option<bool>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Location {
+    city: Option<String>,
+    region: Option<String>,
+    country: Option<String>,
+    continent: Option<String>,
+    region_code: Option<String>,
+    continent_code: Option<String>,
+    latitude: Option<String>,
+    longitude: Option<String>,
+    time_zone: Option<String>,
+    locale_code: Option<String>,
+    metro_code: Option<String>,
+    is_in_european_union: Option<bool>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Network {
+    network: Option<String>,
+    autonomous_system_number: Option<String>,
+    autonomous_system_organization: Option<String>
+}
+
+type Other = serde_json::Map<String, serde_json::Value>;
 
 #[tokio::main]
 async fn main() {
@@ -33,16 +96,20 @@ async fn main() {
 
     let key = get_api_key(&args.key);
     if key == "" {
-        println!("{}", "No key entered".to_string());
+        println!("{}", "You need to enter a key".to_string());
         std::process::exit(0)
     }
 
     let r = get_vpnapi_result(&args.ip, &key).await;
-    // println!("result = {:#?}", r);
 
     let deserialized: VpnApiResult = serde_json::from_str(&r.unwrap()).unwrap();
-    println!("result = {:#?}", deserialized);
 
+
+    // Indent if true
+    match args.pprint {
+        true => println!("{}", serde_json::to_string_pretty(&deserialized).unwrap()),
+        false => println!("{}", serde_json::to_string(&deserialized).unwrap())
+    }
 
 }
 
@@ -56,50 +123,6 @@ fn check_if_valid_ip(s: &String) -> bool {
 
 fn check_if_global_ip(s: &String) -> bool {
     IpAddr::from_str(&s).unwrap().is_global()
-}
-
-type Other = serde_json::Map<String, serde_json::Value>;
-
-#[derive(Deserialize, Debug)]
-struct VpnApiResult {
-    ip: Option<String>,
-    security: Option<Security>,
-    location: Option<Location>,
-    network: Option<Network>,
-    message: Option<String>,
-    #[serde(flatten)]
-    other: Other,
-}
-
-#[derive(Deserialize, Debug)]
-struct Security {
-    vpn: bool,
-    proxy: bool,
-    tor: bool,
-    relay: bool
-}
-
-#[derive(Deserialize, Debug)]
-struct Location {
-    city: String,
-    region: String,
-    country: String,
-    continent: String,
-    region_code: String,
-    continent_code: String,
-    latitude: String,
-    longitude: String,
-    time_zone: String,
-    locale_code: String,
-    metro_code: String,
-    is_in_european_union: String
-}
-
-#[derive(Deserialize, Debug)]
-struct Network {
-    network: String,
-    autonomous_system_number: String,
-    autonomous_system_organization: String
 }
 
 
@@ -118,6 +141,10 @@ async fn get_vpnapi_result(ip: &String, key: &str) -> Result<String, Box<dyn std
 
     Ok(body)
 
+}
+
+fn set_api_key(k: &String) {
+    println!("{}", k)
 }
 
 fn get_api_key(k: &String) -> &str {
